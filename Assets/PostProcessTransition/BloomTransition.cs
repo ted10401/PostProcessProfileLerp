2b4bdfae0368af685d799ab2fb8f5a9e09bfefd7
ted <ted10401@gmail.com>
2019-03-13 17:42:43 +0800

    Add the fast mode transition.

--- Assets/PostProcessTransition/BloomTransition.cs
+++ Assets/PostProcessTransition/BloomTransition.cs
@@ -114,6 +114,25 @@ public class BloomTransition : BaseTransition<Bloom>
         }
         m_dirtIntensity.x = m_fromSettings == null ? 0f : m_fromSettings.dirtIntensity.value;
         m_dirtIntensity.y = m_toSettings == null ? 0f : m_toSettings.dirtIntensity.value;
+
+        if ((m_fromSettings != null && m_fromSettings.fastMode.overrideState) ||
+            (m_toSettings != null && m_toSettings.fastMode.overrideState))
+        {
+            m_tempSettings.fastMode.overrideState = true;
+
+            if(m_toSettings != null)
+            {
+                m_tempSettings.fastMode.value = m_toSettings.fastMode.value;
+            }
+            else if(m_fromSettings != null)
+            {
+                m_tempSettings.fastMode.value = m_fromSettings.fastMode.value;
+            }
+        }
+        else
+        {
+            m_tempSettings.fastMode.overrideState = false;
+        }
     }
 
     public override void Transition(float value)
